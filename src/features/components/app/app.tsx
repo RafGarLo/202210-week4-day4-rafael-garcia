f5cafe4c67c1c0f0c411b836408e5066844a9d8c
Rafael <grafa74@gmail.com>
2022-10-27 18:10:43 +0200

    made initial changes and structure

---
+++ src/features/components/app/app.tsx
@@ -0,0 +1,13 @@
+// import { TodoContextProvider } from '../../context/todo/provider';
+// import { Layout } from '../layout/layout';
+// import { AppRoutes } from '../routes/app.routes';
+
+// export function App() {
+//     return (
+//         <Layout>
+//             <TodoContextProvider>
+//                 <AppRoutes></AppRoutes>
+//             </TodoContextProvider>
+//         </Layout>
+//     );
+// }
